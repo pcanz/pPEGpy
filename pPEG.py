1d94cbf26dc3f0cf3afe98983996eb817ff7943d
pcanz <cashin.peter@gmail.com>
2022-01-05 13:53:38 -0500

    extns
    added extensions
    added options: trace, raw

--- pPEG.py
+++ pPEG.py
@@ -1,6 +1,8 @@
 """
     pPEG is a portable PEG grammar parser generator
 
+    See: <https://github.com/pcanz/pPEG>
+
     This is the only file you need.
 
     pPEG.compile(grammar) -> <Peg>
@@ -55,8 +57,10 @@ class Peg: # parse result...
         return f"{self.err}"
  
 class Env(): # parser machine environment...
-    def __init__(self, code, input):
+    def __init__(self, code, input, extend, trace=False):
         self.code = code
+        self.extend = extend
+
         self.input = input
         self.pos = 0
         self.end = len(input)
@@ -66,17 +70,22 @@ class Env(): # parser machine environment...
         self.depth = -1
         self.max_depth = 100
         self.in_rule = [None]*self.max_depth
+        self.start = [0]*self.max_depth
+        self.stack = [0]*self.max_depth
 
         self.peak_fail = -1
         self.peak_rule = None
         self.peak_expect = None
 
-        self.trace = False
+        self.trace = trace
         self.trace_pos = -1
         self.line_map = None
 
-def _parse(code, input):
-    env = Env(code, input)
+        self.extn_indent = [] # TODO clean-up somehow
+
+
+def _parse(code, input, extend={}, trace=False):
+    env = Env(code, input, extend, trace)
     result = id(code["$start"], env)
     if env.trace:
         print(env.err)
@@ -110,6 +119,8 @@ def id(exp, env):
         return False
     env.depth += 1
     env.in_rule[env.depth] = name
+    env.start[env.depth] = env.pos
+    env.stack[env.depth] = len(env.tree)
     result = expr[0](expr, env)
     env.depth -= 1
     if not result: return False
@@ -145,9 +156,6 @@ def alt(exp, env):
     stack = len(env.tree)
     for arg in exp[1]:
         if arg[0](arg, env): return True
-        # if env.pos > start and env.pos > env.peak_fail:
-        #     env.peak_fail = env.pos
-        #     env.peak_rule = env.in_rule[env.depth]
         if len(env.tree) > stack:
             env.tree = env.tree[0:stack]       
         env.pos = start
@@ -288,6 +296,24 @@ def chs_(exp, env): # ~[x]*
     if count < min: return False
     return True
 
+def extn(exp, env): # [extn, "<xxx>"]
+    ext = exp[1][1:-1].split(' ')
+    key = ext[0]
+    sigil, i = '', 0
+    while i < len(key) and key[i] < 'A': # 0..@
+        sigil += key[i]
+        i += 1
+    key = sigil or key
+    fn = env.extend.get(key) or builtin.get(key)
+    if not fn:
+        env.err += "missing extension: "+exp[1]+"\n"
+        return False
+    try:
+        return fn(exp, env)
+    except Exception as err: # TODO err...
+        env.err += str(err)+"\n"
+        return False
+
 op = {
     "id": id,
     "seq": seq,
@@ -301,11 +327,139 @@ op = {
     "dq_": dq_,
     "chs": chs,
     "chs_": chs_,
+    "extn": extn,
 }
 
 # def eval(exp, env):
 #     # print(exp, exp[0])
 #     return op[exp[0]](exp, env)
+
+# -- extensions ------------------------------------------
+
+def ext_trace(exp, env): # <?> trace...
+    env.trace = True
+    return True
+
+# <infix> ---------------------------------
+
+def infix(exp, env):
+    stack = env.stack[env.depth]
+    idx = stack # first result of rule
+    end = len(env.tree)
+    if end - idx < 3: return True
+    args = env.tree[idx:]
+
+    def pratt(lbp):
+        nonlocal idx, args
+        result = args[idx]
+        idx += 1
+        while idx < end:
+            op = args[idx] # peek
+            rbp = 0
+            if op[0][-3] == '_':  # _xL or _xR
+                x = int(op[0][-2], 32)
+                if op[0][-1] == "L": rbp = (x*2)+1
+                if op[0][-1] == "R": rbp = (x+1)*2
+            if rbp < lbp: break
+            idx += 1 # consume op
+            if rbp%2 == 0: rbp -= 1
+            else: rbp += 1
+            result = [op[1], [result, pratt(rbp)]]
+        return result
+
+    ast = pratt(0)
+    env.tree = env.tree[0:stack]
+    env.tree.append(ast)
+    return True
+
+# <@name> -------------------------------------------------
+
+def same_match(exp, env): # <@name>
+    name = exp[1][2:-1].strip()
+    code = env.code[name]
+    if not code: raise Exception(exp[1]+" undefined rule: "+name)
+    prior = '' # previous name rule result
+    i = len(env.tree)-1
+    while i >= 0:
+        [rule, value] = env.tree[i]
+        if rule == name:
+            prior = value
+            break
+        i -= 1
+    if prior == '': return True # '' empty match deafult (no prior value)
+    if env.input.startswith(prior, env.pos):
+        env.pos += len(prior)
+        return True   
+    return False
+
+
+# <quote> and <quopter> --------------------------------------
+
+def quote(exp, env): # marks <quote>
+    input = env.input
+    start = env.start[env.depth]
+    sot = env.pos # start of text
+    marks = input[start:sot]
+    eot = input.find(marks, sot)
+    if eot == -1: return false
+    env.tree.append(["quote", input[sot:eot]])
+    env.pos = eot+len(marks)
+    return True
+
+def quoter(exp, env): # marks <quoter>
+    input = env.input
+    start = env.start[env.depth]
+    sot = env.pos
+    marks = input[start:sot][::-1] # reverse
+    eot = input.find(marks, sot)
+    if eot == -1: return false
+    env.tree.append(["quoter", input[sot:eot]])
+    env.pos = eot+len(marks)
+    return True
+
+# <indent, <inset>, <undent> ---------------------------------
+
+# TODO uses env.extn_indent, should clean that up somehow
+
+def ext_indent(exp, env):
+    input = env.input;
+    start = env.pos
+    i = start
+    while input[i] == " ": i+=1
+    if i == start:
+        while input[i] == "\t": i+=1
+    if i == start: return False
+    current = env.extn_indent[-1]
+    if current and i-start <= current.length: return False
+    if current and current[0] != input[start]:
+        raise Exception('indent: mixed space & tab')
+    env.extn_indent.push(env.input[start:i])
+    return True
+
+def ext_inset(exp, env):
+    input = env.input
+    start = env.pos
+    i = start
+    while input[i] == " ": i+=1
+    if i == start:
+        while input[i] == "\t": i+=1
+    if i == start: return False
+    return input[start:i] == env.extn_indent[-1]
+
+def ext_undent(exp, env):
+    env.extn_indent.pop()
+    return True
+
+
+builtin = {
+    "?": ext_trace,
+    "@": same_match,
+    "infix": infix,
+    "quote": quote, "quoter": quoter,
+    "indent": ext_indent, "inset": ext_inset, "undent": ext_undent,
+}
+
+
 
 # -- utils ------------------------------------------------
 
@@ -371,6 +525,7 @@ op_display = {
     "dq_": lambda exp: f'"{exp[1]}"',
     "chs": lambda exp: exp[1],
     "chs_": lambda exp: show_chs_(exp),
+    "extn": lambda exp: exp[1],
 }
 
 def display(exp):
@@ -406,13 +561,13 @@ def sfx(min, max):
 
 # -- compile parser code -----------------------------------------------------------
 
-def _compile(ptree): # ptree -> code
+def _compile(ptree, raw=False): # ptree -> code
     code = {}
     for rule in ptree[1]:
         [_rule, [[_id, name], exp]] = rule
         code[name] = exp
     
-    code["$err"] = ""
+    code["$err"] = ''
 
     def emit_id(exp):
         rule = code.get(exp[1])
@@ -470,7 +625,9 @@ def _compile(ptree): # ptree -> code
             icase = True
         else:
             str = str[1:-1]
-        str = bytes(str, "utf-8").decode("unicode_escape")
+        if raw: # _compile arg (why is nonlocal not required?)
+            # TODO unicodes are wrongly escaped, implement pPEG escapes...
+            str = bytes(str, "utf-8").decode("unicode_escape")
         if icase: return [op[exp[0]+'_'], str, True]
         return [op[exp[0]+'_'], str]
 
@@ -486,13 +643,14 @@ def _compile(ptree): # ptree -> code
         "sq": emit_sq_dq,
         "dq": emit_sq_dq,
         "chs": emit_leaf,
+        "extn": emit_leaf,
     }
 
     def optimize(exp):
         return emiter[exp[0]](exp)
 
     for name in code:
-        code[name] = optimize(code[name])
+        if name != "$err": code[name] = optimize(code[name])
 
     [_rule, [[_id, start], _exp]] = ptree[1][0]
     code["$start"] = [op["id"], start]
@@ -502,15 +660,15 @@ pPEG_code = _compile(pPEG_ptree) # ; print(pPEG_code)
 
 # -- pPEG.compile grammar API ----------------------------------------------------------
 
-def compile(grammar):
-    """ pPEG.compile function returns a pPEG.Peg parser object """
-    peg = _parse(pPEG_code, grammar)
+def compile(grammar, extend = {}, raw=False):
+    """ pPEG.compile(grammar) returns a pPEG.Peg parser object """
+    peg = _parse(pPEG_code, grammar, extend)
     if not peg.ok:
         peg.err = "grammar error: "+peg.err
         peg.parse = lambda _ : peg
         return peg
-    code = _compile(peg.ptree)
+    code = _compile(peg.ptree, raw)
     if code["$err"]: return Peg(False, code["$err"], peg, lambda _ : peg)
-    return Peg(True, None, peg, lambda input: _parse(code, input))
+    return Peg(True, None, peg, lambda input, *trace: _parse(code, input, extend, trace))
 
 
